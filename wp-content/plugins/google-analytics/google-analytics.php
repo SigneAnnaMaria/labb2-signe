dde3d1cbb2a526208b8ddc4441d3ad1b8533c183
SigneAnnaMaria <signe.annamaria@gmail.com>
2023-04-21 17:06:09 +0200

    Comments to GA plugin

--- wp-content/plugins/google-analytics/google-analytics.php
+++ wp-content/plugins/google-analytics/google-analytics.php
@@ -6,10 +6,10 @@ Version: 1.0
 Author: Signe Hellquist
 */
 
-// Add Google Analytics tracking code to wp_footer
+// Lägger till Google Analytics tracking code till wp_footer
 function google_analytics_tracking_code()
 {
-    $tracking_id = 'UA-XXXXXXXXX-X'; // Replace with your own Google Analytics tracking ID
+    $tracking_id = 'UA-XXXXXXXXX-X'; // Ersätt med egen Google Analytics tracking ID
 ?>
     <!-- Google Analytics tracking code -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_attr($tracking_id); ?>"></script>
